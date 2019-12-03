aa46cbb390ea1835eae6f51c2429f52d1c617948
zhangzihe <zhangdiandong@sodao.com>
2019-12-03 11:22:44 +0800

    项目提交

---
+++ JsonToExcel/MainFrm.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace JsonToExcel
+{
+    partial class MainFrm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainFrm));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtRoot = new System.Windows.Forms.TextBox();
+            this.btnExport = new System.Windows.Forms.Button();
+            this.txtJsonPath = new System.Windows.Forms.TextBox();
+            this.txtContent = new System.Windows.Forms.TextBox();
+            this.btnFile = new System.Windows.Forms.Button();
+            this.btnPreview = new System.Windows.Forms.Button();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.panel1);
+            this.splitContainer1.Panel1MinSize = 399;
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.txtResult);
+            this.splitContainer1.Panel2MinSize = 500;
+            this.splitContainer1.Size = new System.Drawing.Size(983, 612);
+            this.splitContainer1.SplitterDistance = 399;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.txtRoot);
+            this.panel1.Controls.Add(this.btnExport);
+            this.panel1.Controls.Add(this.txtJsonPath);
+            this.panel1.Controls.Add(this.txtContent);
+            this.panel1.Controls.Add(this.btnFile);
+            this.panel1.Controls.Add(this.btnPreview);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(399, 612);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 412);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 12);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "数组根:";
+            // 
+            // txtRoot
+            // 
+            this.txtRoot.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.txtRoot.Location = new System.Drawing.Point(51, 406);
+            this.txtRoot.Name = "txtRoot";
+            this.txtRoot.Size = new System.Drawing.Size(130, 21);
+            this.txtRoot.TabIndex = 10;
+            this.txtRoot.Text = "hits.hits";
+            // 
+            // btnExport
+            // 
+            this.btnExport.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnExport.Location = new System.Drawing.Point(331, 405);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(66, 23);
+            this.btnExport.TabIndex = 9;
+            this.btnExport.Text = "导出";
+            this.btnExport.UseVisualStyleBackColor = true;
+            this.btnExport.Click += new System.EventHandler(this.btnExport_Click);
+            // 
+            // txtJsonPath
+            // 
+            this.txtJsonPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtJsonPath.Location = new System.Drawing.Point(0, 434);
+            this.txtJsonPath.Multiline = true;
+            this.txtJsonPath.Name = "txtJsonPath";
+            this.txtJsonPath.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtJsonPath.Size = new System.Drawing.Size(399, 178);
+            this.txtJsonPath.TabIndex = 8;
+            this.txtJsonPath.Text = "#请输入要提取的Json路径, 每行一个, 如:\r\n_source.id\r\n_source.aid\r\n_source.personalAccounts[0].pe" +
+    "rsonalWeChatId\r\n\r\n#同时还支持以下操作符:\r\n#  $\t 根对象\r\n#  @\t 当前对象\r\n#  ..\t 递归下降\r\n#  []\t下标操作符\r" +
+    "\n#  *\t通配符\r\n#  ?()\t过滤条件";
+            // 
+            // txtContent
+            // 
+            this.txtContent.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtContent.BackColor = System.Drawing.SystemColors.Window;
+            this.txtContent.Location = new System.Drawing.Point(0, 0);
+            this.txtContent.MaxLength = 999999;
+            this.txtContent.Multiline = true;
+            this.txtContent.Name = "txtContent";
+            this.txtContent.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtContent.Size = new System.Drawing.Size(399, 399);
+            this.txtContent.TabIndex = 7;
+            this.txtContent.Text = resources.GetString("txtContent.Text");
+            // 
+            // btnFile
+            // 
+            this.btnFile.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnFile.Location = new System.Drawing.Point(187, 405);
+            this.btnFile.Name = "btnFile";
+            this.btnFile.Size = new System.Drawing.Size(66, 23);
+            this.btnFile.TabIndex = 6;
+            this.btnFile.Text = "选择文件";
+            this.btnFile.UseVisualStyleBackColor = true;
+            this.btnFile.Click += new System.EventHandler(this.btnFile_Click);
+            // 
+            // btnPreview
+            // 
+            this.btnPreview.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnPreview.Location = new System.Drawing.Point(259, 405);
+            this.btnPreview.Name = "btnPreview";
+            this.btnPreview.Size = new System.Drawing.Size(66, 23);
+            this.btnPreview.TabIndex = 4;
+            this.btnPreview.Text = "预览";
+            this.btnPreview.UseVisualStyleBackColor = true;
+            this.btnPreview.Click += new System.EventHandler(this.btnPreview_Click);
+            // 
+            // txtResult
+            // 
+            this.txtResult.BackColor = System.Drawing.SystemColors.Control;
+            this.txtResult.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtResult.Location = new System.Drawing.Point(0, 0);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtResult.Size = new System.Drawing.Size(580, 612);
+            this.txtResult.TabIndex = 0;
+            // 
+            // MainFrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(983, 612);
+            this.Controls.Add(this.splitContainer1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainFrm";
+            this.Text = "Json转Excel";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnFile;
+        private System.Windows.Forms.Button btnPreview;
+        private System.Windows.Forms.TextBox txtJsonPath;
+        private System.Windows.Forms.TextBox txtContent;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtRoot;
+        private System.Windows.Forms.Button btnExport;
+    }
+}
+
---
+++ JsonToExcel/MainFrm.cs
@@ -0,0 +1,90 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JsonToExcel
+{
+    public partial class MainFrm : Form
+    {
+        public MainFrm()
+        {
+            InitializeComponent();
+            txtContent.MaxLength = int.MaxValue;
+            txtResult.MaxLength = int.MaxValue;
+        }
+
+        private List<string> Result;
+
+        private void btnPreview_Click(object sender, EventArgs e)
+        {
+            var alllines = txtContent.Text.Split(new string[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries).Where(x => !x.StartsWith("#"));
+            var content = string.Join("\r\n", alllines);
+
+            if (string.IsNullOrWhiteSpace(content))
+            {
+                MessageBox.Show("请选择文件或粘贴Json文本");
+                return;
+            }
+
+            var lines = new List<string>();
+            try
+            {
+                var paths = txtJsonPath.Text.Split(new string[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries).Where(x => !x.StartsWith("#"));
+                var arr = string.IsNullOrWhiteSpace(txtRoot.Text) ? JArray.Parse(content) : JObject.Parse(content).SelectToken(txtRoot.Text);
+
+                lines.Add("\r\n" + string.Join(",", paths.Select(x => x.Substring(x.LastIndexOf('.') + 1))));
+                foreach (var jtoken in arr)
+                {
+                    var items = paths.Count() == 0 ? jtoken.Select(x => x.ToString(Formatting.None)) : paths.Select(x => jtoken.SelectToken(x).ToString(Formatting.None));
+                    var line = string.Join(",", items);
+                    lines.Add(line);
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("请检查数据格式:" + ex.Message);
+                return;
+            }
+
+            var prelines = lines.Take(40).Select(x => x.Replace(",", "\r\t")).ToList();
+            prelines.Insert(1, "-----------------------------------------------------------------------------------------");
+            txtResult.Text = "预览仅展示前40条:\r\n";
+            txtResult.Text += string.Join("\r\n", prelines);
+            Result = lines;
+        }
+
+        private void btnFile_Click(object sender, EventArgs e)
+        {
+            var dialog = new OpenFileDialog();
+            dialog.InitialDirectory = "C://";
+            dialog.Filter = "文本文件|*.txt";
+            dialog.RestoreDirectory = false;
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+                txtContent.Text = File.ReadAllText(dialog.FileName);
+        }
+
+        private void btnExport_Click(object sender, EventArgs e)
+        {
+            var dialog = new SaveFileDialog();
+            dialog.InitialDirectory = "C://";
+            dialog.Filter = "表格文件|*.csv";
+            dialog.RestoreDirectory = false;
+            dialog.FileName = "json.csv";
+
+            if (dialog.ShowDialog() != DialogResult.OK)
+                return;
+
+            File.WriteAllLines(dialog.FileName, Result.ToArray());
+        }
+    }
+}
